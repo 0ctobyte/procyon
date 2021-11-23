824129598c08a27b1e31f03568fb8ede4c728738
Sekhar Bhattacharya <1755812+0ctobyte@users.noreply.github.com>
2021-11-23 10:09:32 -0800

    procyon(core): Simplify done and grant signal logic in procyon_ccu_arb

--- rtl/core/procyon_ccu_arb.sv
+++ rtl/core/procyon_ccu_arb.sv
@@ -55,45 +55,45 @@ module procyon_ccu_arb #(
     procyon_onehot2binary #(OPTN_CCU_ARB_DEPTH) ccu_arb_idx_onehot2binary (.i_onehot(ccu_arb_select), .o_binary(ccu_arb_idx));
     procyon_ff #(CCU_ARB_IDX_WIDTH) ccu_arb_idx_r_ff (.clk(clk), .i_en(ccu_arb_state_is_idle), .i_d(ccu_arb_idx), .o_q(ccu_arb_idx_r));
 
-    logic [OPTN_CCU_ARB_DEPTH-1:0] ccu_arb_select_r;
-    procyon_ff #(OPTN_CCU_ARB_DEPTH) ccu_arb_select_r_ff (.clk(clk), .i_en(ccu_arb_state_is_idle), .i_d(ccu_arb_select), .o_q(ccu_arb_select_r));
-
     // CCU FSM
     // CCU can begin transaction if a requestor asserts valid and the FSM is in IDLE
     // CCU will be busy while the BIU is servicing that transaction. When it receives ack from BIU it will signal done to the requestor
     // After one cycle of asserting done, it will return back to the idle state.
     logic [CCU_ARB_STATE_WIDTH-1:0] ccu_arb_state_r;
     logic [CCU_ARB_STATE_WIDTH-1:0] ccu_arb_state_next;
-    logic ccu_arb_done;
-    logic ccu_arb_grant;
+    logic [OPTN_CCU_ARB_DEPTH-1:0] ccu_arb_done;
+    logic [OPTN_CCU_ARB_DEPTH-1:0] ccu_arb_grant;
     logic biu_en;
 
     always_comb begin
         logic any_valid;
         any_valid = (ccu_arb_select != 0);
 
+        ccu_arb_done = '0;
+        ccu_arb_grant = '0;
+
         case (ccu_arb_state_r)
             CCU_ARB_STATE_IDLE: begin
-                ccu_arb_done = 1'b0;
-                ccu_arb_grant = any_valid;
+                ccu_arb_done = '0;
+                ccu_arb_grant = ccu_arb_select;
                 biu_en = 1'b0;
                 ccu_arb_state_next = any_valid ? CCU_ARB_STATE_BUSY : ccu_arb_state_r;
             end
             CCU_ARB_STATE_BUSY: begin
-                ccu_arb_done = i_biu_done;
-                ccu_arb_grant = 1'b0;
+                ccu_arb_done[ccu_arb_idx_r] = i_biu_done;
+                ccu_arb_grant = '0;
                 biu_en = i_ccu_arb_valid[ccu_arb_idx_r];
                 ccu_arb_state_next = i_biu_done ? CCU_ARB_STATE_DONE : ccu_arb_state_r;
             end
             CCU_ARB_STATE_DONE: begin
-                ccu_arb_done = 1'b0;
-                ccu_arb_grant = 1'b0;
+                ccu_arb_done = '0;
+                ccu_arb_grant = '0;
                 biu_en = 1'b0;
                 ccu_arb_state_next = CCU_ARB_STATE_IDLE;
             end
             default: begin
-                ccu_arb_done = 1'b0;
-                ccu_arb_grant = 1'b0;
+                ccu_arb_done = '0;
+                ccu_arb_grant = '0;
                 biu_en = 1'b0;
                 ccu_arb_state_next = CCU_ARB_STATE_IDLE;
             end
@@ -102,16 +102,9 @@ module procyon_ccu_arb #(
 
     procyon_srff #(CCU_ARB_STATE_WIDTH) ccu_arb_state_r_srff (.clk(clk), .n_rst(n_rst), .i_en(1'b1), .i_set(ccu_arb_state_next), .i_reset(CCU_ARB_STATE_IDLE), .o_q(ccu_arb_state_r));
 
-    // Only set the DONE signal for the selected requestor
-    logic [OPTN_CCU_ARB_DEPTH-1:0] ccu_arb_done_select;
-
-    assign ccu_arb_done_select = {(OPTN_CCU_ARB_DEPTH){ccu_arb_done}} & ccu_arb_select_r;
-    procyon_srff #(OPTN_CCU_ARB_DEPTH) o_ccu_arb_done_srff (.clk(clk), .n_rst(n_rst), .i_en(1'b1), .i_set(ccu_arb_done_select), .i_reset('0), .o_q(o_ccu_arb_done));
-
-    logic [OPTN_CCU_ARB_DEPTH-1:0] ccu_arb_grant_select;
-
-    assign ccu_arb_grant_select = {(OPTN_CCU_ARB_DEPTH){ccu_arb_grant}} & ccu_arb_select_r;
-    procyon_srff #(OPTN_CCU_ARB_DEPTH) o_ccu_arb_grant_srff (.clk(clk), .n_rst(n_rst), .i_en(1'b1), .i_set(ccu_arb_grant_select), .i_reset('0), .o_q(o_ccu_arb_grant));
+    // Only set the DONE and GRANT signals for the selected requestor
+    procyon_srff #(OPTN_CCU_ARB_DEPTH) o_ccu_arb_done_srff (.clk(clk), .n_rst(n_rst), .i_en(1'b1), .i_set(ccu_arb_done), .i_reset('0), .o_q(o_ccu_arb_done));
+    procyon_srff #(OPTN_CCU_ARB_DEPTH) o_ccu_arb_grant_srff (.clk(clk), .n_rst(n_rst), .i_en(1'b1), .i_set(ccu_arb_grant), .i_reset('0), .o_q(o_ccu_arb_grant));
 
     procyon_ff #(DC_LINE_WIDTH) o_ccu_arb_data_ff (.clk(clk), .i_en(1'b1), .i_d(i_biu_data), .o_q(o_ccu_arb_data));
 
