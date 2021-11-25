817bd81180f67b09560f61d04e7c2222e0cb3765
Sekhar Bhattacharya <1755812+0ctobyte@users.noreply.github.com>
2021-11-25 12:14:26 -0800

    procyon(tb): Add LSU tests

---
+++ tb/lsu/dut.sv
@@ -0,0 +1,170 @@
+/*
+ * Copyright (c) 2021 Sekhar Bhattacharya
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+`define SRAM_ADDR_WIDTH 20
+`define SRAM_DATA_WIDTH 16
+
+`define WB_CTI_WIDTH    3
+`define WB_BTE_WIDTH    2
+
+module dut #(
+    parameter OPTN_DATA_WIDTH         = 32,
+    parameter OPTN_ADDR_WIDTH         = 32,
+    parameter OPTN_RAT_DEPTH          = 32,
+    parameter OPTN_NUM_IEU            = 1,
+    parameter OPTN_INSN_FIFO_DEPTH    = 3,
+    parameter OPTN_ROB_DEPTH          = 12,
+    parameter OPTN_RS_IEU_DEPTH       = 7,
+    parameter OPTN_RS_LSU_DEPTH       = 5,
+    parameter OPTN_LQ_DEPTH           = 3,
+    parameter OPTN_SQ_DEPTH           = 2,
+    parameter OPTN_VQ_DEPTH           = 3,
+    parameter OPTN_MHQ_DEPTH          = 3,
+    parameter OPTN_DC_CACHE_SIZE      = 1024,
+    parameter OPTN_DC_LINE_SIZE       = 32,
+    parameter OPTN_DC_WAY_COUNT       = 1,
+    parameter OPTN_WB_DATA_WIDTH      = 32,
+    parameter OPTN_WB_ADDR_WIDTH      = 32,
+    parameter OPTN_WB_SRAM_BASE_ADDR  = 0
+)(
+    input  logic                        clk,
+    input  logic                        n_rst,
+
+    // SRAM interface
+    output logic [`SRAM_ADDR_WIDTH-1:0] o_sram_addr,
+    input  logic [`SRAM_DATA_WIDTH-1:0] i_sram_dq,
+    output logic [`SRAM_DATA_WIDTH-1:0] o_sram_dq,
+    output logic                        o_sram_ce_n,
+    output logic                        o_sram_we_n,
+    output logic                        o_sram_oe_n,
+    output logic                        o_sram_ub_n,
+    output logic                        o_sram_lb_n,
+
+    // FIXME: To test if simulations pass/fail
+    output logic [OPTN_DATA_WIDTH-1:0]  o_sim_tp,
+    output logic                        o_sim_retire,
+
+    // FIXME: Temporary instruction cache interface
+    input  logic [OPTN_DATA_WIDTH-1:0]  i_ic_insn,
+    input  logic                        i_ic_valid,
+    output logic [OPTN_ADDR_WIDTH-1:0]  o_ic_pc,
+    output logic                        o_ic_en
+);
+
+    timeunit 1ns;
+    timeprecision 1ns;
+
+    localparam RAT_IDX_WIDTH    = $clog2(OPTN_RAT_DEPTH);
+    localparam WB_DATA_SIZE     = OPTN_WB_DATA_WIDTH / 8;
+
+    logic wb_clk;
+    logic wb_rst;
+    logic wb_ack;
+    logic [OPTN_WB_DATA_WIDTH-1:0] wb_data_i;
+    logic wb_cyc;
+    logic wb_stb;
+    logic wb_we;
+    logic [`WB_CTI_WIDTH-1:0] wb_cti;
+    logic [`WB_BTE_WIDTH-1:0] wb_bte;
+    logic [WB_DATA_SIZE-1:0] wb_sel;
+    logic [OPTN_WB_ADDR_WIDTH-1:0] wb_addr;
+    logic [OPTN_WB_DATA_WIDTH-1:0] wb_data_o;
+
+    logic sram_we_n;
+/* verilator lint_off UNOPTFLAT */
+    logic [`SRAM_DATA_WIDTH-1:0] sram_dq;
+/* verilator lint_on  UNOPTFLAT */
+
+/* verilator lint_off UNUSED */
+    // FIXME: FPGA debugging output
+    logic rob_redirect;
+    logic [OPTN_ADDR_WIDTH-1:0] rob_redirect_addr;
+    logic rat_retire_en;
+    logic [RAT_IDX_WIDTH-1:0] rat_retire_rdst;
+    logic [OPTN_DATA_WIDTH-1:0] rat_retire_data;
+/* verilator lint_on  UNUSED */
+
+    assign o_sim_retire = rat_retire_en;
+
+    assign wb_clk = clk;
+    assign wb_rst = ~n_rst;
+
+    assign sram_dq = sram_we_n ? i_sram_dq : 'z;
+    assign o_sram_we_n = sram_we_n;
+    assign o_sram_dq = sram_dq;
+
+    procyon #(
+        .OPTN_DATA_WIDTH(OPTN_DATA_WIDTH),
+        .OPTN_ADDR_WIDTH(OPTN_ADDR_WIDTH),
+        .OPTN_RAT_DEPTH(OPTN_RAT_DEPTH),
+        .OPTN_NUM_IEU(OPTN_NUM_IEU),
+        .OPTN_INSN_FIFO_DEPTH(OPTN_INSN_FIFO_DEPTH),
+        .OPTN_ROB_DEPTH(OPTN_ROB_DEPTH),
+        .OPTN_RS_IEU_DEPTH(OPTN_RS_IEU_DEPTH),
+        .OPTN_RS_LSU_DEPTH(OPTN_RS_LSU_DEPTH),
+        .OPTN_LQ_DEPTH(OPTN_LQ_DEPTH),
+        .OPTN_SQ_DEPTH(OPTN_SQ_DEPTH),
+        .OPTN_VQ_DEPTH(OPTN_VQ_DEPTH),
+        .OPTN_MHQ_DEPTH(OPTN_MHQ_DEPTH),
+        .OPTN_DC_CACHE_SIZE(OPTN_DC_CACHE_SIZE),
+        .OPTN_DC_LINE_SIZE(OPTN_DC_LINE_SIZE),
+        .OPTN_DC_WAY_COUNT(OPTN_DC_WAY_COUNT),
+        .OPTN_WB_DATA_WIDTH(OPTN_WB_DATA_WIDTH),
+        .OPTN_WB_ADDR_WIDTH(OPTN_WB_ADDR_WIDTH)
+    ) procyon_inst (
+        .clk(clk),
+        .n_rst(n_rst),
+        .o_sim_tp(o_sim_tp),
+        .o_rob_redirect(rob_redirect),
+        .o_rob_redirect_addr(rob_redirect_addr),
+        .o_rat_retire_en(rat_retire_en),
+        .o_rat_retire_rdst(rat_retire_rdst),
+        .o_rat_retire_data(rat_retire_data),
+        .i_ic_insn(i_ic_insn),
+        .i_ic_valid(i_ic_valid),
+        .o_ic_pc(o_ic_pc),
+        .o_ic_en(o_ic_en),
+        .i_wb_clk(wb_clk),
+        .i_wb_rst(wb_rst),
+        .i_wb_ack(wb_ack),
+        .i_wb_data(wb_data_i),
+        .o_wb_cyc(wb_cyc),
+        .o_wb_stb(wb_stb),
+        .o_wb_we(wb_we),
+        .o_wb_cti(wb_cti),
+        .o_wb_bte(wb_bte),
+        .o_wb_sel(wb_sel),
+        .o_wb_addr(wb_addr),
+        .o_wb_data(wb_data_o)
+    );
+
+    sram_wb #(
+        .OPTN_WB_DATA_WIDTH(OPTN_WB_DATA_WIDTH),
+        .OPTN_WB_ADDR_WIDTH(OPTN_WB_ADDR_WIDTH),
+        .OPTN_BASE_ADDR(OPTN_WB_SRAM_BASE_ADDR)
+    ) sram_wb_inst (
+        .i_wb_clk(wb_clk),
+        .i_wb_rst(wb_rst),
+        .i_wb_cyc(wb_cyc),
+        .i_wb_stb(wb_stb),
+        .i_wb_we(wb_we),
+        .i_wb_cti(wb_cti),
+        .i_wb_bte(wb_bte),
+        .i_wb_sel(wb_sel),
+        .i_wb_addr(wb_addr),
+        .i_wb_data(wb_data_o),
+        .o_wb_data(wb_data_i),
+        .o_wb_ack(wb_ack),
+        .o_sram_ce_n(o_sram_ce_n),
+        .o_sram_oe_n(o_sram_oe_n),
+        .o_sram_we_n(sram_we_n),
+        .o_sram_lb_n(o_sram_lb_n),
+        .o_sram_ub_n(o_sram_ub_n),
+        .o_sram_addr(o_sram_addr),
+        .io_sram_dq(sram_dq)
+    );
+
+endmodule
---
+++ tb/lsu/sc_main.cpp
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2019 Sekhar Bhattacharya
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "Vdut.h"
+#include "verilated_vcd_sc.h"
+
+#include "BootRom.h"
+#include "Sram.h"
+
+#define SRAM_DATA_WIDTH    (16)
+#define SRAM_ADDR_WIDTH    (20)
+#define SRAM_SIZE          (1 << (SRAM_ADDR_WIDTH+1))
+
+#define PASS               (0x4a33)
+#define FAIL               (0xfae1)
+
+bool ends_with(const std::string& str, const std::string& suffix) {
+    return (str.size() >= suffix.size() && str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0);
+}
+
+int sc_main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);
+    Verilated::traceEverOn(true);
+
+    if (argc < 2) {
+        std::cerr << "ERROR: No binary or hex file specified" << std::endl;
+    }
+
+    std::string top_name("top");
+    sc_trace_file *tf = sc_create_vcd_trace_file("sysc");
+
+    sc_clock clk("clk", sc_time(1, SC_NS));
+    sc_signal<bool> n_rst(0);
+
+    sc_trace(tf, clk, top_name+".clk");
+    sc_trace(tf, n_rst, top_name+".n_rst");
+
+    sc_signal<uint32_t> sim_tp;
+    sc_signal<bool> sim_retire;
+    sc_signal<uint32_t> ic_insn;
+    sc_signal<bool> ic_valid;
+    sc_signal<uint32_t> ic_pc;
+    sc_signal<bool> ic_en;
+
+    sc_signal<uint32_t> sram_addr;
+    sc_signal<uint32_t> sram_dq_i;
+    sc_signal<uint32_t> sram_dq_o;
+    sc_signal<bool> sram_ce_n;
+    sc_signal<bool> sram_we_n;
+    sc_signal<bool> sram_oe_n;
+    sc_signal<bool> sram_ub_n;
+    sc_signal<bool> sram_lb_n;
+
+    Sram<SRAM_SIZE> sram("sram");
+    sram.trace_all(tf, top_name);
+    sram.i_sram_addr(sram_addr);
+    sram.i_sram_dq(sram_dq_o);
+    sram.o_sram_dq(sram_dq_i);
+    sram.i_sram_ce_n(sram_ce_n);
+    sram.i_sram_we_n(sram_we_n);
+    sram.i_sram_oe_n(sram_oe_n);
+    sram.i_sram_ub_n(sram_ub_n);
+    sram.i_sram_lb_n(sram_lb_n);
+
+    BootRom bootrom("bootrom");
+    bootrom.trace_all(tf, top_name);
+    bootrom.clk(clk);
+    bootrom.i_ic_en(ic_en);
+    bootrom.i_ic_pc(ic_pc);
+    bootrom.o_ic_valid(ic_valid);
+    bootrom.o_ic_insn(ic_insn);
+
+    Vdut dut("dut");
+    dut.clk(clk);
+    dut.n_rst(n_rst);
+    dut.o_sram_addr(sram_addr);
+    dut.i_sram_dq(sram_dq_i);
+    dut.o_sram_dq(sram_dq_o);
+    dut.o_sram_ce_n(sram_ce_n);
+    dut.o_sram_we_n(sram_we_n);
+    dut.o_sram_oe_n(sram_oe_n);
+    dut.o_sram_ub_n(sram_ub_n);
+    dut.o_sram_lb_n(sram_lb_n);
+    dut.o_sim_tp(sim_tp);
+    dut.o_sim_retire(sim_retire);
+    dut.i_ic_insn(ic_insn);
+    dut.i_ic_valid(ic_valid);
+    dut.o_ic_pc(ic_pc);
+    dut.o_ic_en(ic_en);
+
+    std::string rom_file(argv[1]);
+    std::string suffix_str("hex");
+    if (ends_with(rom_file, suffix_str)) {
+        bootrom.load_hex(rom_file);
+        sram.load_hex(rom_file);
+    } else {
+        bootrom.load_bin(rom_file);
+        sram.load_bin(rom_file);
+    }
+
+    VerilatedVcdSc tfp;
+    dut.trace(&tfp, 100);
+    tfp.open("dut.vcd");
+
+    uint64_t retired_insns = 0, cycles = 0;
+    while (sim_tp != PASS && sim_tp != FAIL && sc_get_status() != SC_STOPPED) {
+        if (sc_time_stamp() >= sc_time(1, SC_NS)) n_rst = 1;
+        sc_start(1, SC_NS);
+        if (sim_retire) retired_insns++;
+        if (n_rst) cycles++;
+    }
+
+    double cpi = (double)cycles/(double)retired_insns;
+
+    std::cout << "\nINSTRUCTIONS: " << std::dec << retired_insns
+        << " CYCLES: " << std::dec << cycles
+        << " CPI: " << std::dec << cpi << std::endl;
+
+    int err = 0;
+    if (sim_tp == PASS && sc_get_status() != SC_STOPPED) {
+        std::cout << "\n\n" << "*********************************    PASS    *********************************" << std::endl;
+        err = cpi;
+    } else {
+        std::cout << "\n\n" << "*********************************    FAIL    *********************************" << std::endl;
+        err = 0;
+    }
+
+    dut.final();
+    sc_close_vcd_trace_file(tf);
+    tfp.close();
+    return err;
+}
